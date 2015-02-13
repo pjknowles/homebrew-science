90807bc5c36cab48fbc156489066e2a2de3f4e9e
Peter Knowles <KnowlesPJ@Cardiff.ac.uk>
2015-02-13 09:20:45 +0000

    jmol 14.2.12
    Jmol is a free, open source molecule viewer for students, educators, and researchers in chemistry and biochemistry. It is cross-platform, running on Windows, Mac OS X, and Linux/Unix systems. This package provides the Jmol application.

---
+++ jmol.rb
@@ -0,0 +1,30 @@
+class Jmol < Formula
+  homepage "http://www/jmol.org"
+  url "https://downloads.sourceforge.net/jmol/Jmol/Version%2014.2/Version%2014.2.12/Jmol-14.2.12_2015.02.11-binary.zip"
+  sha1 "f2f6f39b1c89d59ce624b3078ad19a0eef1f7d0f"
+
+  head do
+    url "https://jmol.svn.sourceforge.net/svnroot/jmol/trunk/Jmol"
+    depends_on :ant
+  end
+
+  depends_on :java
+
+  def install
+    system "ant" if build.head?
+    mv "jmol", "jmol.original"
+    (buildpath/"jmol").write <<-EOS.undent
+      #!/bin/sh
+      JMOL_HOME=#{prefix} exec #{prefix}/jmol.sh "$*"
+    EOS
+    chmod "+x", %w[jmol jmol.sh]
+    bin.install "jmol"
+    prefix.install "jmol.sh", Dir["*.jar"]
+    prefix.install Dir["build/*.jar"] if build.head?
+  end
+
+  test do
+    system "jmol", "-n"
+  end
+end
+
